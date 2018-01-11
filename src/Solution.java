f144da6acba5ef162171bcf94d24b26d4f51ef56
Arvis Abromovics <info@vsmedio.lv>
2018-01-11 20:14:17 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,74 @@
+
+import java.util.List;
+
+/* 
+Таблица
+Измени класс TableInterfaceWrapper так, чтобы он стал Wrapper-ом для ATableInterface.
+Метод setModel должен вывести в консоль количество элементов в новом листе перед обновлением модели.
+Метод getHeaderText должен возвращать текст в верхнем регистре - используйте метод toUpperCase().
+
+
+Требования:
+1. Класс TableInterfaceWrapper должен реализовывать интерфейс ATableInterface.
+2. Класс TableInterfaceWrapper должен инициализировать в конструкторе поле типа ATableInterface.
+3. Метод setModel() должен вывести в консоль количество элементов в новом листе перед обновлением модели.
+4. Метод getHeaderText() должен возвращать текст в верхнем регистре.
+5. Метод setHeaderText() должен устанавливать текст для заголовка без изменений.
+
+
+public class Solution {
+    public class TableInterfaceWrapper {
+
+    }
+
+    public interface ATableInterface {
+        void setModel(List rows);
+
+        String getHeaderText();
+
+        void setHeaderText(String newHeaderText);
+    }
+
+    public static void main(String[] args) {
+    }
+}
+
+*/
+
+public class Solution {
+    public class TableInterfaceWrapper implements ATableInterface {
+
+        ATableInterface component;
+
+        TableInterfaceWrapper (ATableInterface atable) {
+            this.component = atable;
+        }
+
+        @Override
+        public void setModel(List rows) {
+            System.out.println(rows.size());
+            this.component.setModel(rows);
+        }
+
+        @Override
+        public String getHeaderText() {
+            return this.component.getHeaderText().toUpperCase();
+        }
+
+        @Override
+        public void setHeaderText(String newHeaderText) {
+            this.component.setHeaderText(newHeaderText);
+        }
+    }
+
+    public interface ATableInterface {
+        void setModel(List rows);
+
+        String getHeaderText();
+
+        void setHeaderText(String newHeaderText);
+    }
+
+    public static void main(String[] args) {
+    }
+}
